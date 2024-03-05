e86ccee0f73090fa6e45eb4b479f0f6b752e2344
jummy123 <jummy123@users.noreply.github.com>
2024-03-05 21:07:16 +0000

    Add convenience migration for local deployment

--- db/migrations/000002_create_db_users.up.sql
+++ db/migrations/000002_create_db_users.up.sql
@@ -5,4 +5,8 @@ GRANT SELECT ON public."Schema" TO web_anon;
 GRANT SELECT ON public."SchemaName" TO web_anon;
 
 CREATE ROLE authenticator NOINHERIT LOGIN PASSWORD 'mysecretpassword';  -- TODO: Move password out.
+ALTER ROLE authenticator SET pgrst.db_aggregates_enabled = 'true';
 GRANT web_anon TO authenticator;
+
+ALTER ROLE authenticator SET statement_timeout TO '10s';
+ALTER ROLE web_anon SET statement_timeout TO '1s';
